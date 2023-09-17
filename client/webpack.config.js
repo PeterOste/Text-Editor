8d53c8ae89de514162316e162f9949d279120619
unknown <peter.ostertag99@gmail.com>
2023-09-17 16:20:41 -0500

    Add CSS loaders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,6 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,12 +17,33 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Webpack plugin that generates our html file and injects our bundles. 
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Contact Cards'
+      }),
     ],
 
     module: {
       rules: [
-        
+        // CSS loaders
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        // JavaScript loaders
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Babel-loader, necessary for ES6
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
