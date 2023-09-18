5110648e43a4948f5b1e46557146bdeb4fcac377
unknown <peter.ostertag99@gmail.com>
2023-09-18 16:44:40 -0500

    Add get all method to db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,6 +16,28 @@ const initdb = async () =>
 export const putDb = async (content) => console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  // Open jate database with version 1
+  const jateDb = await openDB('jate', 1);
+
+  // Create readonly for jate object
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  // Access to jate object within the transaction
+  const store = tx.objectStore('jate');
+
+  // Retrieve all data from jate object
+  const request = store.getAll();
+
+  // Wait for the request to complete and then store the result in the result variable
+  const result = await request;
+
+  console.log('result.value', result);
+
+  // Return retrieved data as the result
+  return result;
+};
 
 initdb();
