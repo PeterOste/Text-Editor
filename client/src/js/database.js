758b58513a2e9ef216d94f50db529e51b1c06f5e
unknown <peter.ostertag99@gmail.com>
2023-09-18 16:54:00 -0500

    Add put method to db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,31 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Adding content to the database');
+
+  // Open jate database with version 1
+  const jateDb = await openDB('jate', 1);
+
+  // Create readonly for jate object
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  // Access to jate object within the transaction
+  const store = tx.objectStore('jate');
+
+  try {
+    // Add content to jate object with add method, a key will be generated
+    const key = await store.add(content);
+
+    console.log(`Content added with key: ${key}`);
+
+    // Complete transaction
+    await tx.done;
+  } catch (error) {
+    // Error handling
+    console.error('There was an error adding content to the database:', error);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
