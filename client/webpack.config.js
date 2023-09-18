12a76e84760366715cb108896f26782fdb5210f8
unknown <peter.ostertag99@gmail.com>
2023-09-18 16:16:33 -0500

    Add asset caching

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -23,13 +21,13 @@ module.exports = () => {
         filename: 'index.html',
       }),
 
-      // Inject a service worker
+      // Inject service worker
       new InjectManifest({
         swSrc: './src-sw.js', // Path to service worker file
         swDest: 'src-sw.js', // Output file for service worker
       }),
 
-      // Generate a Manifest file
+      // Generate Manifest file
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
